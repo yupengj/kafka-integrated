693c0e45d6a0caf9b361b5340786ef80897dc754
yupengj <jyp10@foxmail.com>
2020-08-14 13:37:30 +0800

    增加日期转换 transforms

--- src/main/java/com/gantang/kafka/connect/transforms/TimestampConverter.java
+++ src/main/java/com/gantang/kafka/connect/transforms/TimestampConverter.java
@@ -1,23 +1,111 @@
 package com.gantang.kafka.connect.transforms;
 
+import org.apache.kafka.common.cache.Cache;
+import org.apache.kafka.common.cache.LRUCache;
+import org.apache.kafka.common.cache.SynchronizedCache;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.data.Timestamp;
 import org.apache.kafka.connect.transforms.Transformation;
+import org.apache.kafka.connect.transforms.util.SchemaUtil;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
+import static org.apache.kafka.connect.transforms.util.Requirements.requireStruct;
+
+/**
+ * 转换所有 schema name 等于 org.apache.kafka.connect.data.Timestamp 的字段，将值转换成 yyyy-MM-dd 日期格式
+ *
+ * @param <R>
+ */
 public class TimestampConverter<R extends ConnectRecord<R>> implements Transformation<R> {
 
-    //org.apache.kafka.connect.data.Timestamp
+    private static final String TIMESTAMP_TYPE = "org.apache.kafka.connect.data.Timestamp";
+    private static final String PURPOSE = "converting all timestamp formats";
+    private static final String FORMAT = "yyyy-MM-dd";
+    private final SimpleDateFormat format = new SimpleDateFormat(FORMAT);
 
-    @Override
-    public R apply(R record) {
-        return null;
-    }
+    private Cache<Schema, Schema> schemaUpdateCache;
 
     @Override
     public ConfigDef config() {
-        return null;
+        return new ConfigDef();
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+        schemaUpdateCache = new SynchronizedCache<>(new LRUCache<>(32));
+    }
+
+    @Override
+    public R apply(R record) {
+        if (operatingSchema(record) == null) {
+            return record;
+        } else {
+            // 只转换有 schema
+            return applyWithSchema(record);
+        }
+    }
+
+    private R applyWithSchema(R record) {
+        final Schema schema = operatingSchema(record);
+        final Struct value = requireStruct(operatingValue(record), PURPOSE);
+        Schema updatedSchema = schemaUpdateCache.get(value.schema());
+        if (updatedSchema == null) {
+            SchemaBuilder builder = SchemaUtil.copySchemaBasics(schema, SchemaBuilder.struct());
+            for (Field field : schema.fields()) {
+                if (TIMESTAMP_TYPE.equals(field.schema().name())) {
+                    builder.field(field.name(), Schema.STRING_SCHEMA);
+                } else {
+                    builder.field(field.name(), field.schema());
+                }
+            }
+            if (schema.isOptional()) {
+                builder.optional();
+            }
+            if (schema.defaultValue() != null) {
+                Struct updatedDefaultValue = applyValueWithSchema((Struct) schema.defaultValue(), builder);
+                builder.defaultValue(updatedDefaultValue);
+            }
+            updatedSchema = builder.build();
+            schemaUpdateCache.put(schema, updatedSchema);
+        }
+        Struct updatedValue = applyValueWithSchema(value, updatedSchema);
+        return newRecord(record, updatedSchema, updatedValue);
+    }
+
+    private Struct applyValueWithSchema(Struct value, Schema updatedSchema) {
+        Struct updatedValue = new Struct(updatedSchema);
+        for (Field field : value.schema().fields()) {
+            final Object updatedFieldValue;
+            if (TIMESTAMP_TYPE.equals(field.schema().name())) {
+                updatedFieldValue = convertTimestamp(value.get(field));
+            } else {
+                updatedFieldValue = value.get(field);
+            }
+            updatedValue.put(field.name(), updatedFieldValue);
+        }
+        return updatedValue;
+    }
+
+    private Object convertTimestamp(Object timestamp) {
+        Date date;
+        if (timestamp instanceof Long) {
+            date = Timestamp.toLogical(Timestamp.SCHEMA, (Long) timestamp);
+        } else if (timestamp instanceof Date) {
+            date = (Date) timestamp;
+        } else {
+            return timestamp;
+        }
+        synchronized (format) {
+            return format.format(date);
+        }
     }
 
     @Override
@@ -25,8 +113,15 @@ public class TimestampConverter<R extends ConnectRecord<R>> implements Transform
 
     }
 
-    @Override
-    public void configure(Map<String, ?> configs) {
+    private Schema operatingSchema(R record) {
+        return record.valueSchema();
+    }
 
+    private Object operatingValue(R record) {
+        return record.value();
+    }
+
+    private R newRecord(R record, Schema updatedSchema, Object updatedValue) {
+        return record.newRecord(record.topic(), record.kafkaPartition(), record.keySchema(), record.key(), updatedSchema, updatedValue, record.timestamp());
     }
 }
--- src/test/java/com/gantang/kafka/connect/transforms/TimestampConverterTest.java
+++ src/test/java/com/gantang/kafka/connect/transforms/TimestampConverterTest.java
@@ -1,37 +1,69 @@
 package com.gantang.kafka.connect.transforms;
 
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.data.Timestamp;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 
 /**
  * 转换日期
  */
 public class TimestampConverterTest {
 
-    private static final String PATTERN1 = "yyyy-MM-dd HH:mm:ss";
-    private static final String PATTERN2= "yyyy-MM-dd";
+    private TimestampConverter<SinkRecord> timestampConverter = new TimestampConverter<>();
+
+    @Before
+    public void before() {
+        timestampConverter.configure(null);
+    }
 
     @Test
     public void testFormat() {
-        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN2);
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         final Date date = Timestamp.toLogical(Timestamp.SCHEMA, 1597225795891L);
         final String format = simpleDateFormat.format(date);
         System.out.println(format);
     }
 
     @Test
+    public void testSchema() {
+        final SchemaBuilder int64 = SchemaBuilder.int64();
+        int64.name("org.apache.kafka.connect.data.Timestamp").version(1);
+        final Schema schema = SchemaBuilder.struct().field("MD_MATERIAL_ID", Schema.INT64_SCHEMA).field("MATERIAL_NUM", Schema.STRING_SCHEMA).field("IS_COLOR", Schema.BOOLEAN_SCHEMA)
+                .field("TEST_ABC", Schema.FLOAT64_SCHEMA).field("CREATED_DATE", int64.build()).build();
+        System.out.println(schema);
+    }
+
+    @Test
     public void apply() {
-    }
+        final SchemaBuilder int64 = SchemaBuilder.int64();
+        int64.name("org.apache.kafka.connect.data.Timestamp").version(1);
+        final Schema schema = SchemaBuilder.struct().field("MD_MATERIAL_ID", Schema.INT64_SCHEMA).field("MATERIAL_NUM", Schema.STRING_SCHEMA).field("IS_COLOR", Schema.BOOLEAN_SCHEMA)
+                .field("TEST_ABC", Schema.FLOAT64_SCHEMA).field("CREATED_DATE", int64.build()).field("UPDATED_DATE", int64.build()).build();
 
-    @Test
-    public void config() {
-    }
+        final Struct value = new Struct(schema);
+        value.put("MD_MATERIAL_ID", 1597225795891L);
+        value.put("MATERIAL_NUM", "1234");
+        value.put("IS_COLOR", true);
+        value.put("TEST_ABC", 23.45d);
+        value.put("CREATED_DATE", new Date(1597225795891L));
+        value.put("UPDATED_DATE", new Date(1597225795891L));
 
-    @Test
-    public void configure() {
+        String topic = "ibom.mstdata.md_material";
+        final SinkRecord record = new SinkRecord(topic, 0, null, null, schema, value, 0);
+        final ConnectRecord apply = timestampConverter.apply(record);
+        final Struct value1 = (Struct) apply.value();
+        final Schema schema1 = apply.valueSchema();
+        Assert.assertEquals(schema1.field("CREATED_DATE").schema(), Schema.STRING_SCHEMA);
+        Assert.assertEquals(value1.get("CREATED_DATE"), "2020-08-12");
+        Assert.assertEquals(value1.get("UPDATED_DATE"), "2020-08-12");
     }
 }
